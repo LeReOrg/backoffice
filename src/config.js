b4414bf0446672a7dcd3a4a96200910da8241366
Vinh <vinh1593570@gmail.com>
2021-08-01 13:50:06 +0700

    add orderstatus

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const configs = {
+  api: "https://staging-lereappserver.herokuapp.com/api/v1",
+};
+
+export default configs;
--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,11 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
+import MenuBookIcon from '@material-ui/icons/MenuBook';
 import Notifications from "@material-ui/icons/Notifications";
 import DashboardPage from "views/Dashboard/Dashboard.js";
 import UserProfile from "views/UserProfile/UserProfile.js";
+import OrderStatusPage from "views/OrderStatus/OrderStatusPage";
+
 import TableList from "views/TableList/TableList.js";
 import NotificationsPage from "views/Notifications/Notifications.js";
 const dashboardRoutes = [
@@ -11,6 +14,13 @@ const dashboardRoutes = [
     name: "Dashboard",
     icon: Dashboard,
     component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/orderStatus",
+    name: "Order Status",
+    icon: MenuBookIcon,
+    component: OrderStatusPage,
     layout: "/admin"
   },
   {
